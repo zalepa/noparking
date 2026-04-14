7569be20d8ccc15d2eae2e3bba4cdb24c9f25d65
George Zalepa <george.zalepa@gmail.com>
2026-04-14 09:18:27 -0400

    add static homepage

---
+++ app/helpers/pages_helper.rb
@@ -0,0 +1,25 @@
+module PagesHelper
+  FEATURE_ICONS = {
+    "photo_evidence"   => "M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z M15 13a3 3 0 11-6 0 3 3 0 016 0z",
+    "location_dispatch" => "M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z M15 11a3 3 0 11-6 0 3 3 0 016 0z",
+    "dashboards"       => "M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z",
+    "role_access"      => "M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z",
+    "notifications"    => "M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9",
+    "open_source"      => "M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+  }.freeze
+
+  ROLE_DOT_CLASSES = {
+    "residents" => "bg-blue-500",
+    "officers"  => "bg-emerald-500",
+    "managers"  => "bg-amber-500",
+    "admins"    => "bg-purple-500"
+  }.freeze
+
+  def feature_icon_path(key)
+    FEATURE_ICONS.fetch(key)
+  end
+
+  def role_dot_class(key)
+    ROLE_DOT_CLASSES.fetch(key)
+  end
+end
